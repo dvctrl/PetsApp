4e8903bebd54f533af85c8b97b7c585ffd9405b8
dvctrl <davidmouzinho.contato@gmail.com>
2023-02-16 22:01:59 -0300

    second commit

---
+++ app/src/main/java/br/com/etecia/pets/MenuSecundario_Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.pets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuSecundario_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_secundario_layout);
+    }
+}
